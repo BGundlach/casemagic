8805586c2db5d3b75963e74dbb3df89442c7509f
Brian Gundlach <GundlachComputing@gmail.com>
2016-11-18 13:35:21 -0800

    Fixed Case Notes on Tracking

--- casemagic-script.js
+++ casemagic-script.js
@@ -1,21 +1,21 @@
 // ==UserScript==
 // @name         CaseMagic
-// @namespace    http://tampermonkey.net/
+// @namespace    https://raw.githubusercontent.com/BGundlach/casemagic/master/casemagic-script.js
 // @version      0.1
-// @description  tools for case magic
+// @description  open an entire class of trackingsheets
 // @author       Brian Gundlach
 // @match        https://casemagic.net/WATCH/home/
 // @grant        none
 // ==/UserScript==
 
-function getConsumerTrackingSheet(name){
+function getConsumerTrackingSheet(array name){
 	$('input#search_box').attr({value:name});
 	searchCaseload();
 	$('table.case_table a[title*="towards case goals"').click();
 	$('a')[1].click();
 }
-function getConsumerTrackingSheets(names){
-	for (var i = 0; i < names.length; i++){
+function getConsumerTrackingSheets(names[]){
+	for (var i = 0; i < names.length(); i++){
 	setTimeout(getConsumerTrackingSheet(names[i]),2000);
 	}
 }
@@ -24,4 +24,27 @@ function loadAdvocateGroup(AdvocateGroup){
 	for (var i = 0; i < AdvocateGroup.length; i++){
 		loadTrackingSheet(AdvocateGroup[i][0],AdvocateGroup[i][1],"","");
 	}
+}
+function go(){
+	loadAdvocateGroup(AdvocateGroup)
+}
+function loadCaseNote4Tracking (consumerID, name, i, k, j, l, m, n, o) {
+	loadCaseNote(consumerID, name, '',0)
+}
+
+addJS_Node (loadCaseNote4Tracking);
+
+function addJS_Node (text, s_URL, funcToRun, runOnLoad) {
+    var D                                   = document;
+    var scriptNode                          = D.createElement ('script');
+    if (runOnLoad) {
+        scriptNode.addEventListener ("load", runOnLoad, false);
+    }
+    scriptNode.type                         = "text/javascript";
+    if (text)       scriptNode.textContent  = text;
+    if (s_URL)      scriptNode.src          = s_URL;
+    if (funcToRun)  scriptNode.textContent  = '(' + funcToRun.toString() + ')()';
+
+    var targ = D.getElementsByTagName ('head')[0] || D.body || D.documentElement;
+    targ.appendChild (scriptNode);
 }
