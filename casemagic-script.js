09224ac701df1c7d08ad84d7ea39a049e03b1028
Brian Gundlach <GundlachComputing@gmail.com>
2016-12-12 15:01:38 -0800

    Update casemagic-script.js

--- casemagic-script.js
+++ casemagic-script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CaseMagic
 // @namespace    https://raw.githubusercontent.com/BGundlach/casemagic/master/casemagic-script.js
-// @version      0.3.1
+// @version      0.4
 // @description  open an entire class of trackingsheets
 // @author       Brian Gundlach
 // @match        https://casemagic.net/WATCH/home/
@@ -14,22 +14,22 @@ function getConsumerTrackingSheet(name){
 	$('table.case_table a[title*="towards case goals"').click();
 	$('a')[1].click();
 }
-function getConsumerTrackingSheets(names[]){
+function getConsumerTrackingSheets(names){
 	for (var i = 0; i < names.length(); i++){
 	setTimeout(getConsumerTrackingSheet(names[i]),2000);
 	}
 }
-var AdvocateGroup = [[171, "Josiah"],[20,"Kevin Bass"],[158, "Annika"],[39,"Carol"],[140,"Breanne"],[183,"Karissa"],[180,"Leahana"]]
+var AdvocateGroup = [[171, "Josiah"],[20,"Kevin Bass"],[158, "Annika"],[39,"Carol"],[140,"Breanne"],[183,"Karissa"],[180,"Leahana"]];
 function loadAdvocateGroup(AdvocateGroup){
 	for (var i = 0; i < AdvocateGroup.length; i++){
 		loadTrackingSheet(AdvocateGroup[i][0],AdvocateGroup[i][1],"","");
 	}
 }
 function go(event){
-	loadAdvocateGroup(AdvocateGroup)
+	loadAdvocateGroup(AdvocateGroup);
 }
 function go(){
-	loadAdvocateGroup(AdvocateGroup)
+	loadAdvocateGroup(AdvocateGroup);
 }
 var zNode       = document.createElement ('div');
 zNode.innerHTML = '<button id="myButton" type="button">'
